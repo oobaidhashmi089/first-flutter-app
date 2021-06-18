4a001c724766d8244d9fc08c710f44ca69e0daa2
oobaidhashmi089 <79754399+oobaidhashmi089@users.noreply.github.com>
2021-06-18 13:45:35 -0700

    fist commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+          child: Container(
+        child: Center(child: Text("Hello Iam Obaid")),
+      )),
+    );
+  }
+}
