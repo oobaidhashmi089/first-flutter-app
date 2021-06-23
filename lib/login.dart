2e072995fb62c1e7b50841de7ecd16f9840d28e2
oobaidhashmi089 <79754399+oobaidhashmi089@users.noreply.github.com>
2021-06-23 14:36:48 -0700

    day5-login-page-completed

--- lib/login.dart
+++ lib/login.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text("data")),
+       
+      color: Colors.white,
+      
+      child: Column(
+        children: [
+          Center(
+            
+            child: Image.network(
+              "https://t3.ftcdn.net/jpg/03/39/70/90/240_F_339709048_ZITR4wrVsOXCKdjHncdtabSNWpIhiaR7.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          SizedBox(
+            height: 40.0,
+          ),
+          Center(
+              child: Text(
+            "welcome",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+          SizedBox(
+            height: 40.0,
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter Username", labelText: "Username:"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Password", labelText: "Password:"),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Login"))
+              ],
+            ),
+          ),
+          //Image.asset("assets/login.png"),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
 import 'login.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,18 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch:Colors.deepPurple),
-      darkTheme: ThemeData(primarySwatch: Colors.deepOrange),
-
-
-     routes: {
-       "/": (context)=> login(),
-       "/home": (context)=> Home(),
-       "/Login": (context)=>login(),
-
-     }, 
-    );
+      theme: ThemeData(primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily,
     
+      
+      ),
+      darkTheme: ThemeData(primarySwatch: Colors.deepOrange),
+      
+      routes: {
+        "/": (context) => login(),
+        "/home": (context) => Home(),
+        "/Login": (context) => login(),
+      },
+    );
   }
 }
