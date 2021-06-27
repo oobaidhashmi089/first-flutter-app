50faf8b8c4c05b18677b8bd058e16aaaa8298789
oobaidhashmi089 <79754399+oobaidhashmi089@users.noreply.github.com>
2021-06-27 14:02:27 -0700

    editAppBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_ob/widgets/drawer.dart';
 
 class Home extends StatelessWidget {
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
         appBar: AppBar(
-            // backgroundColor: Color(0xff310263),
             title: Center(
-                child: Center(
-          child: Image.asset("asset/home_page.png"),
-        ))),
+                child: Image.asset(
+              'asset/windows_10_store.png',
+              height: 100,
+              width: 100,
+            )),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(
+                    CupertinoIcons.shopping_cart,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    // do something
+                  }),
+            ]),
+        // title: new Row(
+        //   mainAxisAlignment: MainAxisAlignment.end,
+        //   children: [
+        //     Text("*****",),
+        //     Container(
+
+        //         padding: const EdgeInsets.all(8.0),
+        //         child: Image.asset(
+        //           "images/add_shopping_cart_64px.png",
+        //           fit: BoxFit.contain,
+        //           height: 32,
+        //         ))
+        //   ],
+        // ),
+        // backgroundColor: Color(0xff310263),
+        //
+
         drawer: Mdrawer(),
         body: Center(
           child: SizedBox(
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -6,10 +6,12 @@ class Mthemes {
       primaryColor: Color(0xFFF7F7F7),
       fontFamily: GoogleFonts.lato().fontFamily,
       appBarTheme: AppBarTheme(
+        
         //  color: Colors.white,
         iconTheme: IconThemeData(color: Colors.black),
         elevation: 0.0,
         textTheme: Theme.of(context).textTheme,
+        
       ));
   static ThemeData darkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
