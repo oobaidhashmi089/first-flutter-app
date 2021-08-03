1237795a7c5ff56623f82d8a1850ebf99b620e7e
oobaidhashmi089 <79754399+oobaidhashmi089@users.noreply.github.com>
2021-08-03 11:52:02 -0700

    done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_application_ob/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
 import 'package:flutter_application_ob/model/catapp.dart';
 import 'package:flutter_application_ob/widgets/drawer.dart';
 import 'package:flutter_application_ob/widgets/items.dart';
-import 'dart:convert';
 
 class Home extends StatefulWidget {
   @override
@@ -34,49 +38,106 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
-        appBar: AppBar(
-            title: Center(
-                child: Image.asset(
-              'asset/windows_10_store.png',
-              height: 100,
-              width: 100,
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Center(
+              child: Image.asset(
+            'asset/windows_10_store.png',
+            height: 100,
+            width: 100,
+          )),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+                  CupertinoIcons.shopping_cart,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  // do something
+                }),
+          ]),
+      drawer: Mdrawer(),
+      body: SafeArea(
+        child: Container(
+            color: Colors.white,
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                "Saint-Étienne APP".text.bold.xl5.black.center.make(),
+                "Atlético mineiro".text.xl.blueGray500.make(),
+                if (Mitems.items != null && Mitems.items.isNotEmpty)
+                  CatalogList().expand()
+                else
+                  Center(child: CircularProgressIndicator(color: Colors.black)),
+              ],
             )),
-            actions: <Widget>[
-              IconButton(
-                  icon: Icon(
-                    CupertinoIcons.shopping_cart,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    // do something
-                  }),
-            ]),
-        drawer: Mdrawer(),
-        body: Padding(
-            padding: const EdgeInsets.all(38.0),
-            child: (Mitems.items != null && Mitems.items.isNotEmpty)
-                ? GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 15,
-                        crossAxisSpacing: 15),
-                    itemBuilder: (context, index) {
-                      var item = Mitems.items[index];
+      ),
+    ));
+  }
+}
 
-                      return Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12)),
-                          child: Card(
-                              child: GridTile(
-                            child: Image.network(item.img),
-                            header: Text(item.name),
-                            footer: Text(item.price.toString()),
-                          )));
-                    })
-                : Center(
-                    child: CircularProgressIndicator(
-                        valueColor:
-                            new AlwaysStoppedAnimation<Color>(Colors.black)),
-                  ))));
+class CatalogList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: Mitems.items.length,
+      itemBuilder: (context, index) {
+        final catalog = Mitems.items[index];
+        return CatalogItmes(catalog: catalog);
+      },
+    );
+  }
+}
+
+class CatalogItmes extends StatelessWidget {
+  final Items catalog;
+  const CatalogItmes({
+    Key key,
+    @required this.catalog,
+  })  : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatalogImage(image: catalog.img),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catalog.name.text.color(Mthemes.darkblue).bold.make(),
+            catalog.desc.text.textStyle(context.captionStyle).make(),
+            ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                buttonPadding: EdgeInsets.zero,
+                children: [
+                  "\$${catalog.price}".text.bold.xl.make(),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Mthemes.darkblue),
+                          shape: MaterialStateProperty.all(StadiumBorder())),
+                      child: Text("BUY"))
+                ])
+          ],
+        ))
+      ],
+    )).white.rounded.square(150).make().p16();
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+  const CatalogImage({Key key, @required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image).box.white.rounded.p16.make().p16().w40(context);
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,15 +4,16 @@ import 'package:google_fonts/google_fonts.dart';
 class Mthemes {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primaryColor: Color(0xFFF7F7F7),
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
-        
         //  color: Colors.white,
         iconTheme: IconThemeData(color: Colors.black),
         elevation: 0.0,
         textTheme: Theme.of(context).textTheme,
-        
       ));
   static ThemeData darkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
+
+  static Color darkblue = Color(0xff000080);
+  static Color creamcolor = Color(0xffe6e5d5);
 }
